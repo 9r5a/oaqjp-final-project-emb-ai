658531ca26e749e8f633437e2d07bb651b60cd58
Musab Hussein Mohamed <police49r5a@gmail.com>
2024-08-07 19:16:50 +0300

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template, request, jsonify
+'''  import work modules  '''
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+'''  fun d.s  ''' 
     return render_template('index.html')
 
 @app.route('/emotionDetector', methods=['POST'])
 def emotion_detector_route():
+'''  fun d.s  ''' 
     if request.method == 'POST':
         statement = request.form.get('statement')
         result = emotion_detector(statement)
         if result['dominant_emotion'] is None:
-            return "Invalid text! Please try again."
+            print("Invalid text! Please try again.")
         else:
             return render_template('result.html', result=result)
 
